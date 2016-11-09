03d0e1d9ce9375c6f0c812725930b40dcc1f953a
PaulWintz-THINK\PaulWintz <paulandhisviolin@gmail.com>
2016-11-09 07:33:38 -0800

    Spellcheck

---
+++ PWintz_SpellChecker_F16/src/spellchecker/BasicSpellChecker.java
@@ -0,0 +1,171 @@
+package spellchecker;
+
+import java.io.*;
+import java.util.regex.*;
+
+import org.apache.commons.io.*;
+
+public class BasicSpellChecker implements SpellChecker {
+
+	private static final String REGEX = "\\b[\\w']+\\b";
+
+	BasicDictionary dictionary = new BasicDictionary();
+
+	Pattern regex = Pattern.compile(REGEX);
+
+	Matcher matcher;
+
+
+	String text;
+
+	int index;
+
+
+	/**
+	 * Replaces the current dictionary with words imported from the specified text file. Words in the file are in
+	 * lexicographical (ASCII) order, one word per line.
+	 * 
+	 * @param filename
+	 *            Name (possibly including a path) of the file to import.
+	 * @throws Exception
+	 */
+	public void importDictionary(String filename) throws Exception {
+		dictionary.importFile(filename);
+	}
+
+
+	/**
+	 * Loads the specified file from a previously saved dictionary tree file. The file format is text, with one word per
+	 * line.
+	 * 
+	 * @param filename
+	 *            Name (possibly including a path) of the file to load from.
+	 * @throws Exception
+	 */
+	public void loadDictionary(String filename) throws Exception {
+		dictionary.load(filename);
+	}
+
+
+	/**
+	 * Saves the dictionary tree to the specified file in preorder. The file format is text, with one word per line.
+	 * 
+	 * @param filename
+	 *            Name (possibly including a path) of the file to save to. If the file exists, it is overwritten.
+	 * @throws Exception
+	 */
+	public void saveDictionary(String filename) throws Exception {
+		dictionary.save(filename);
+	}
+
+
+	/**
+	 * Loads the specified text document.
+	 * 
+	 * @param filename
+	 * @throws Exception
+	 */
+	public void loadDocument(String filename) throws Exception {
+		text = FileUtils.readFileToString(new File(filename));
+		matcher = regex.matcher(text);
+	}
+
+
+	/**
+	 * Saves the specified text document.
+	 * 
+	 * @param filename
+	 * @throws Exception
+	 */
+	public void saveDocument(String filename) throws Exception {
+		FileUtils.write(new File(filename), text);
+	}
+
+
+	/**
+	 * 
+	 * @return Returns the text in the document.
+	 */
+	public String getText() {
+		return text;
+	}
+
+
+	/**
+	 * Starts/continues a spell check of the text. Use the regular expression below to match words (it's not great, but
+	 * it's simple and works OK for basic text).
+	 * 
+	 * <pre>
+	 * \b[\w']+\b
+	 * </pre>
+	 * 
+	 * The method returns when the first unknown word is located or when the end of the document is reached (whichever
+	 * comes first). The index of the character after the unknown word is retained for use as the starting index for
+	 * subsequent calls to spellCheck where continueFromPrevious is true.
+	 * 
+	 * @param continueFromPrevious
+	 *            If false, a new spell check is started from the first character of the document. If true, the spell
+	 *            check continues from it's current location.
+	 * @return If no unknown word is found this method returns null. Otherwise, it returns a String array organized as
+	 *         follows:
+	 * 
+	 *         <pre>
+	 *         [0] = Unknown word 
+	 *         [1] = Index of start of unknown word.  The index is the position within the entire document.
+	 *         [2] = Preceeding word in the dictionary .  If the unknown word is before all words in the dictionary, this element should be "".
+	 *         [3] = Succeeding word in the dictionary.  If the unknown word is after all words in the dictionary, this element should be "".
+	 *              e.g. 
+	 *         [0] = "spelm"
+	 *         [1] = "224"
+	 *         [2] = "spells" 
+	 *         [3] = "spelt"
+	 *         </pre>
+	 */
+	public String[] spellCheck(boolean continueFromPrevious) {
+		int start = 0;
+		int end = 0;
+
+
+		while (matcher.find(start)) {
+			start = matcher.start();
+			end = matcher.end();
+
+			String word = text.substring(start, end);
+			String[] words = dictionary.find(word);
+			if (words != null)
+				return words;
+		}
+		;
+
+
+	}
+
+
+	/**
+	 * Adds the given word to the dictionary's tree
+	 * 
+	 * @param word
+	 */
+	public void addWordToDictionary(String word) {
+		dictionary.add(word);
+	}
+
+
+	/**
+	 * Replaces text in the document from startIndex to just before endIndex with the given replacementText.
+	 * 
+	 * NOTE: Be sure to update your spell checker index by adding the difference between the length of the replacement
+	 * text and the length of the text that was replaced.
+	 * 
+	 * @param startIndex
+	 *            Index of the first character to replace.
+	 * @param endIndex
+	 *            Index of the character <b>after</b> the last character to replace.
+	 * @param replacementText
+	 */
+	public void replaceText(int startIndex, int endIndex, String replacementText) {
+		StringBuilder. text = "oeijr";
+		text.replac
+	}
+
+}
--- PWintz_SpellChecker_F16/src/spellchecker/BinaryTree.java
+++ PWintz_SpellChecker_F16/src/spellchecker/BinaryTree.java
@@ -1,5 +1,180 @@
 package spellchecker;
 
-public class BinaryTree {
+import java.util.*;
 
+public class BinaryTree {
+	private BinaryTreeNode root;
+
+	private int count = 0;
+
+
+	public BinaryTree() {
+
+	}
+
+
+	public BinaryTree(String[] words) {
+		for (String word : words) {
+			add(word);
+		}
+	}
+
+
+	public BinaryTree(ArrayList<String> words) {
+		for (String word : words) {
+			add(word);
+		}
+	}
+
+
+	/**
+	 * Checks if word1 is lexigraphically prior to word2.
+	 * 
+	 * @param word1
+	 * @param word2
+	 * @return True if word1 comes before word2, false if word1 comes after word2
+	 * @throws IllegalArgumentException
+	 *             if the words are equal.
+	 */
+	private static boolean areWordsInOrder(String word1, String word2) throws IllegalArgumentException {
+		int compare = word1.compareTo(word2);
+
+		if (compare < 0) {
+			// word1 comes before word2
+			return true;
+		} else if (compare > 0) {
+			// word1 comes after word2
+			return false;
+		} else {
+			throw new IllegalArgumentException("Words are equal: " + word1 + word2);
+		}
+	}
+
+
+	public void add(String word) {
+		count++;
+
+		word = word.trim();
+
+		if (root == null) {
+			root = new BinaryTreeNode(word);
+			return;
+		}
+
+		BinaryTreeNode cur = root;
+
+		while (true) {
+			if (areWordsInOrder(word, cur.value)) {
+				if (cur.left != null) {
+					cur = cur.left;
+					continue;
+				} else {
+					cur.left = new BinaryTreeNode(word);
+					return;
+				}
+			} else {
+				if (cur.right != null) {
+					cur = cur.right;
+					continue;
+				} else {
+					cur.right = new BinaryTreeNode(word);
+					return;
+				}
+			}
+		}
+
+	}
+
+
+	/**
+	 * @param word
+	 * @return If the word is <b>found</b> this method returns <b>null</b>. Otherwise, it returns a String array
+	 *         organized as follows:
+	 * 
+	 *         <pre>
+	 *         [0] = Preceding word in the dictionary 
+	 *         [1] = Succeeding word in the dictionary 
+	 *         
+	 *              e.g. if the unknown word was "spelm", the result might be:
+	 *              
+	 *         [0] = "spells" 
+	 *         [1] = "spelt"
+	 *         
+	 *         If there is no preceding or succeeding word in the dictionary, set the element to "".
+	 *         </pre>
+	 */
+	public String[] find(String word) {
+		word = word.trim();
+
+		BinaryTreeNode cur = root;
+
+		Stack<BinaryTreeNode> stack = new Stack<>();
+
+
+		while (cur != null) {
+			stack.push(cur);
+
+			if (word.equals(cur.value)) {
+				// Item is found!
+				return null;
+			}
+
+			if (areWordsInOrder(word, cur.value)) {
+				// word comes lexigraphically before cursor
+				cur = cur.left;
+			} else {
+				// word comes lexigraphically after cursor
+				cur = cur.right;
+			}
+		}
+
+		String[] neighbors = new String[2];
+
+		BinaryTreeNode leftCur = cur;
+		while (neighbors[0] != null && neighbors[1] != null) {
+			while (leftCur.left == null) {
+
+			}
+
+		}
+
+		return null;
+	}
+
+
+	public ArrayList<String> transverseInPreorder() {
+		ArrayList<String> list = new ArrayList<>();
+		Stack<BinaryTreeNode> rightBranchesStack = new Stack<>();
+
+		BinaryTreeNode cur = root;
+		while (cur != null) {
+			list.add(cur.value);
+
+			// If there is a right branch to the current node, add it to the stack
+			if (cur.right != null) {
+				rightBranchesStack.add(cur.right);
+			}
+
+			if (cur.left != null) {
+				cur = cur.left;
+			} else if (!rightBranchesStack.isEmpty()) {
+				cur = rightBranchesStack.pop();
+			} else {
+				return list;
+			}
+		}
+
+		throw new RuntimeException("Programming error. THis line should not be reached.");
+	}
+
+
+	public int getCount() {
+		return count;
+
+	}
+
+
+	public BinaryTreeNode getRoot() {
+		return root;
+	}
 }
